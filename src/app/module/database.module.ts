d7af7282509ab77e19d7e9cfe1d300c1a74012c7
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2025-11-06 19:54:07 -0500

    feat: :recycle: refactorizar conexion a base de datos en database.module.ts
    refactorizar conexion a base de datos en database.module.ts
    
    refactorizar conexion a base de datos en database.module.ts

--- src/app/module/database.module.ts
+++ src/app/module/database.module.ts
@@ -3,6 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ENV_VARS, EnvironmentClass } from 'environments/env-config';
 
+const databaseConfig = (env: ConfigService<EnvironmentClass>) => ({
+  type: env.get<any>(ENV_VARS.DB_TYPE),
+  host: env.get(ENV_VARS.DB_HOST),
+  port: env.get(ENV_VARS.DB_PORT),
+  username: env.get(ENV_VARS.DB_USERNAME),
+  password: env.get(ENV_VARS.DB_PASSWORD),
+  database: env.get<string>(ENV_VARS.DB_NAME),
+  schema: env.get(ENV_VARS.DB_SCHEMA),
+  synchronize: env.get(ENV_VARS.DB_SYNCHRONIZE),
+  ssl: env.get(ENV_VARS.DB_SSL),
+  autoLoadEntities: env.get(ENV_VARS.DB_AUTO_LOAD_ENTITIES),
+});
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,18 +24,7 @@ import { ENV_VARS, EnvironmentClass } from 'environments/env-config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (env: ConfigService<EnvironmentClass>) => ({
-        type: env.get<any>(ENV_VARS.DB_TYPE),
-        host: env.get(ENV_VARS.DB_HOST),
-        port: env.get(ENV_VARS.DB_PORT),
-        username: env.get(ENV_VARS.DB_USERNAME),
-        password: env.get(ENV_VARS.DB_PASSWORD),
-        database: env.get<string>(ENV_VARS.DB_NAME),
-        schema: env.get(ENV_VARS.DB_SCHEMA),
-        synchronize: env.get(ENV_VARS.DB_SYNCHRONIZE),
-        ssl: env.get(ENV_VARS.DB_SSL),
-        autoLoadEntities: env.get(ENV_VARS.DB_AUTO_LOAD_ENTITIES),
-      }),
+      useFactory: (env: ConfigService<EnvironmentClass>) => databaseConfig(env),
     }),
   ],
 })
