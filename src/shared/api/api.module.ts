4a9d5050aaa41d6aa69fd6db65a24d05f6676775
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2026-05-19 21:17:59 -0500

    fix: :bug: borrar carpeta api

--- src/shared/api/api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from '@nestjs/common';
-import { ApiGatewayService } from '@/shared/api/general-api/http-gateway-axios.api';
-
-@Global()
-@Module({
-  providers: [ApiGatewayService],
-  exports: [ApiGatewayService],
-})
-export class ApiModule {}
